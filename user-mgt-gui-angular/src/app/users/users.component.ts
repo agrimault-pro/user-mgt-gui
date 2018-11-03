48f46e17c0baa723ce2fa601b6b33cac1c204429
agrimault-pro <42966675+agrimault-pro@users.noreply.github.com>
2018-11-03 16:52:13 +0100

    Add some static elements in the GUI

--- user-mgt-gui-angular/src/app/users/users.component.ts
+++ user-mgt-gui-angular/src/app/users/users.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  userName = 'John';
+  userLastName = 'Doe';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  getLastName() {
+    return this.userLastName;
+  }
 }
