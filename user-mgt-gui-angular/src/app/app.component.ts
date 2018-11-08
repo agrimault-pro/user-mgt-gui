2e135be4c9c320622634573acdbe689a3387f207
agrimault-pro <42966675+agrimault-pro@users.noreply.github.com>
2018-11-08 20:17:02 +0100

    Remove counter (observable interval)

--- user-mgt-gui-angular/src/app/app.component.ts
+++ user-mgt-gui-angular/src/app/app.component.ts
@@ -1,32 +1,15 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/Rx';
-import { Subscription } from 'rxjs/Rx';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy{
+export class AppComponent implements OnInit {
   
-  seconds: number;
-  counterSubscription: Subscription;
-
   constructor() {
   }
 
   ngOnInit() {
-    //FIXME NE MARCHE PAS !!! "seconds" is not displayed
-    const counter = Observable.interval(1000);
-    this.counterSubscription = counter.subscribe(
-      (value: number) => {
-        this.seconds = value;
-      }
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.counterSubscription.unsubscribe();
   }
 }
